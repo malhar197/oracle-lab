d4d68de2e1f726db1133e9a8d45d095590cf20f4
Malhar Dave <malharvdave@gmail.com>
2020-06-14 22:16:42 -0400

    setStockInfo, getStockprice, getStockVolume functions added

--- src/stocks.js
+++ src/stocks.js
@@ -5,15 +5,22 @@ import{STOCK_ORACLE_ABI, STOCK_ORACLE_ADDRESS} from './quotecontract.js';
 const web3 = new Web3("http://localhost:7545");
 
 class Stocks extends React.Component {
+
+
 	constructor() {
 		super();
 		this.handleChange = this.handleChange.bind(this);
 		this.getStockInfo = this.getStockInfo.bind(this);
+		this.setStockInfo = this.setStockInfo.bind(this);
+		this.getStockPrice = this.getStockPrice.bind(this);
+		this.getStockVolume = this.getStockVolume.bind(this);
 		this.state = {
 			tempSymbol: '',
 			symbol: '',
 			price: '',
-			volume: ''
+			volume: '',
+			priceFromContract: '',
+			volumeFromContract: ''
 		}
 	}
 
@@ -40,6 +47,37 @@ getStockInfo = event => {
  	.catch(console.log);
 }
 
+setStockInfo = async (event) => {
+	event.preventDefault();
+	try {
+
+            const accounts = await web3.eth.getAccounts();
+			const stockQuote = new web3.eth.Contract(STOCK_ORACLE_ABI, STOCK_ORACLE_ADDRESS);
+            console.log("Account 0 = ", accounts[0]);
+            const retval = await stockQuote.methods.setStock(web3.utils.fromAscii(this.state.symbol), parseInt(this.state.price * 100), parseInt(this.state.volume)).send({ from: accounts[0] });
+
+        }
+        catch (error) {
+            console.log('setStock failed', error);
+        }
+}
+
+getStockPrice = async (event) => {
+	const accounts = await web3.eth.getAccounts();
+	const stockQuote = new web3.eth.Contract(STOCK_ORACLE_ABI, STOCK_ORACLE_ADDRESS);
+    console.log("Account 0 = ", accounts[0]);
+    let retrievedPrice = await stockQuote.methods.getStockPrice(web3.utils.fromAscii(this.state.symbol)).call();
+        this.setState({ priceFromContract: parseFloat(retrievedPrice / 100) });
+}
+
+getStockVolume = async (event) => {
+	const accounts = await web3.eth.getAccounts();
+	const stockQuote = new web3.eth.Contract(STOCK_ORACLE_ABI, STOCK_ORACLE_ADDRESS);
+    console.log("Account 0 = ", accounts[0]);
+    let retrievedVolume = await stockQuote.methods.getStockVolume(web3.utils.fromAscii(this.state.symbol)).call();
+        this.setState({ volumeFromContract: retrievedVolume });
+}
+
 render() {
 	return (
 		<div>
@@ -55,7 +93,7 @@ render() {
 		<br /> 
 		<div className = 'buttons'>
 		<form onSubmit={this.setSymbol}>
-		Enter Stock Symbol: <input type="text" value={this.state.tempSymbol} onChange={this.handleChange} />
+		Enter Stock Symbol: <input type="text" value={this.state.tempymbol} onChange={this.handleChange} />
 		<input type="submit" value="Submit" />
 		</form>
 		<br />
@@ -63,11 +101,23 @@ render() {
 		<br />
 		<h3> Stock info from API </h3>
 		<div>
-		<ul>
-		<li> Symbol: {this.state.symbol} </li>
-		<li> Price: {this.state.price} </li>
-		<li> Volume: {this.state.volume}</li>
-		</ul>
+		
+		Symbol: {this.state.symbol} 
+		Price: {this.state.price} 
+		 Volume: {this.state.volume}
+		
+		</div>
+		<button type="button" onClick={this.setStockInfo}> Set stock information in smart contract </button>
+		<br />
+		<button type = "button" onClick={this.getStockPrice}> Retrieve stock price from smart contract </button>
+		<br />
+		<button type = "button" onClick={this.getStockVolume}> Retrieve stock volume from smart contract </button>
+		<br />
+		<h3> Stock info from Oracle </h3>
+		<div>
+		<li> Stock price retrieved from smart contract: {this.state.priceFromContract} </li>
+		<br />
+		<li>Stock volume retrieved from smart contract: {this.state.volumeFromContract}</li>
 		</div>
 		</div>
 		</div>
